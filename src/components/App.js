1f5adf65823baa8ccfdb2bf071d2e7a1d3c36b44
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2018-11-23 17:05:34 +0000

    add limit with button to show additional memes

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            memeLimit: 10
+        }
+    }
     render() {
         return (
             <div>
                 <h2>Generate your memes here</h2>
-                { this.props.memes.map((meme, index) => {
+                { this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
                         return (
                             <h4 key={index}>{meme.name}</h4>
                         )
                     })
                 }
+                <div className="button" onClick={() => {
+                    this.setState({ memeLimit: this.state.memeLimit + 10 })
+                }}>
+                    Load more memes...
+                </div>
             </div>
         )
     }
