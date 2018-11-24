c3cc46017f93339ca7a3ad36f07a6689b27fdd4c
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2018-11-24 22:19:13 +0000

    added styling

--- src/components/App.js
+++ src/components/App.js
@@ -20,43 +20,46 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                <h2>Generate your memes here</h2>
-                <MyMemes/>
-                <Form inline>
-                    <FormGroup>
-                        <ControlLabel>Caption Top</ControlLabel>
-                        {' '}
-                        <FormControl
-                            type="text"
-                            onChange={ event => this.setState({ topText: event.target.value  })}
-                        />
-                        {' '}
-                    </FormGroup>
-                    <FormGroup>
-                        {' '}
-                        <ControlLabel>&nbsp;&nbsp;Caption Bottom</ControlLabel>
-                        {' '}
-                        <FormControl
-                            type="text"
-                            onChange={ event => this.setState({ bottomText: event.target.value  })}
-                        />
-                    </FormGroup>
-                </Form>
-                { this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
-                        return (
-                            <MemeItem
-                                key={index}
-                                meme={meme}
-                                topText={this.state.topText}
-                                bottomText={this.state.bottomText}
+                <div className="container">
+                    <h2>Generate your memes here</h2>
+                    <h4>Add both captions and select an image</h4>
+                    <MyMemes/>
+                    <Form inline>
+                        <FormGroup>
+                            <ControlLabel>Caption Top&nbsp;</ControlLabel>
+                            {' '}
+                            <FormControl
+                                type="text"
+                                onChange={ event => this.setState({ topText: event.target.value  })}
                             />
-                        )
-                    })
-                }
-                <div className="button" onClick={() => {
-                    this.setState({ memeLimit: this.state.memeLimit + 10 })
-                }}>
-                    Load more memes...
+                            {' '}
+                        </FormGroup>
+                        <FormGroup>
+                            {' '}
+                            <ControlLabel>&nbsp;&nbsp;Caption Bottom&nbsp;</ControlLabel>
+                            {' '}
+                            <FormControl
+                                type="text"
+                                onChange={ event => this.setState({ bottomText: event.target.value  })}
+                            />
+                        </FormGroup>
+                    </Form>
+                    { this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
+                            return (
+                                <MemeItem
+                                    key={index}
+                                    meme={meme}
+                                    topText={this.state.topText}
+                                    bottomText={this.state.bottomText}
+                                />
+                            )
+                        })
+                    }
+                    <div className="button" onClick={() => {
+                        this.setState({ memeLimit: this.state.memeLimit + 10 })
+                    }}>
+                        Load more memes...
+                    </div>
                 </div>
             </div>
         )
